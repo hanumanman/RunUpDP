8e506cc8a748092e1c2e87b5c43f5d49fcc5c6a6
Itztla <hanumanman37@gmail.com>
2023-04-04 22:49:33 +0700

    minor text change

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,8 @@ module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
-    console.log(`Logged in as ${client.user.username} on private test server`);
+    console.log(
+      `Hello! Logged in as ${client.user.username} on private test server`
+    );
   },
 };
